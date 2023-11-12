11ba0cbc3ae4d2625b7d61b30149c1794eaf6042
Patrick Casimiro <patrickcasimiroz@gmail.com>
2023-11-12 14:34:25 -0300

    feat/refactor: substituindo o root por theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,23 @@
+import { ThemeProvider } from 'styled-components'
+
 import Header from './components/Cabecalho'
 import Hero from './components/Hero'
 import ListaVagas from './containers/ListaVagas'
 
-import './global.css' 
+import './global.css'
 import GlobalStyle, { Container } from './styles'
+import themaColors from './theme/theme'
 
 function App() {
   return (
-    <>
+    <ThemeProvider theme={themaColors}>
       <GlobalStyle />
       <Header />
       <Hero />
       <Container>
         <ListaVagas />
       </Container>
-    </>
+    </ThemeProvider>
   )
 }
 
---
+++ src/theme/theme.ts
@@ -0,0 +1,6 @@
+const themaColors = {
+  primary: '#a77727d',
+  secondary: '#f9f5e7'
+}
+
+export default themaColors
