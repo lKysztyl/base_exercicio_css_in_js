9565a07ec4df2bae125cb21632576d7150b4ace7
Patrick Casimiro <patrickcasimiroz@gmail.com>
2023-11-12 16:11:15 -0300

    refactory(Hero): adicionando styled-components no lugar do CSS

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -1,13 +1,13 @@
-import styles from './Hero.module.css'
+import { ClassHero, ContainerH2, HeroTitle } from './styles'
 
 const Hero = () => (
-  <form className={styles.form}>
-    <div className="container">
-      <h2 className={styles.heroTitle}>
+  <ClassHero>
+    <ContainerH2>
+      <HeroTitle>
         As melhores vagas para tecnologia, design e artes visuais.
-      </h2>
-    </div>
-  </form>
+      </HeroTitle>
+    </ContainerH2>
+  </ClassHero>
 )
 
 export default Hero
